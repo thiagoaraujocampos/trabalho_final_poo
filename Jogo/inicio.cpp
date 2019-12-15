7155564b4966a399c70bf6bdd8fa4df3468b6c07
SaraOwO <sarahferreirah1@gmail.com>
2019-12-15 17:36:20 -0300

    alteraçoes

--- Jogo/inicio.cpp
+++ Jogo/inicio.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 
 using namespace std;
+using namespace sf;
 
 int main(){
 
@@ -12,22 +13,38 @@ int main(){
         //erro
     }
 
-    sf::Text text;
+    Texture background;
+    if (!background.loadFromFile("../Imagens/menu.png")){
+        //erro
+    }
+
+    Sprite fundo(background);
+
+
+    //---------------------------------------------------------------------------- titulo
+
+    Text text;
     text.setFont(font); 
+    Color color(0,153,0);
 
-    text.setString("ranking");
-    text.setCharacterSize(70); // in pixels, not points!
+    text.setFillColor(color);
+    text.setString("Chernocolor");
+    text.setCharacterSize(70); // fonte e tamanho da fonte
 
-    text.setFillColor(sf::Color::White);
+    text.setOutlineColor(Color::White); //cor da borda e tamanho
+    text.setOutlineThickness(3);
 
-     while (window.isOpen()){
+    text.setOrigin(Vector2f(-250,-30)); //coloca o titulo no centro
+
+    while (window.isOpen()){
         sf::Event event;
         while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear(sf::Color::Black);
+        window.clear(Color::Black);
+        window.draw(fundo);
         window.draw(text);
         window.display();
     }
